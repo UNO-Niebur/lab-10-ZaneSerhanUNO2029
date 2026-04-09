92c215c6c1d9adf8d773a2494610075f5bb4e0dc
ZaneSerhanUNO2029 <12106466@nebraska.edu>
2026-04-09 17:51:56 +0000

    in progress

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 #Name:
 #Date:
 #Assignment:
+import csv
+import matplotlib as plt
+trial = []
+reactionTime = []
+
+with open("reaction_time_data.csv", "r") as file:
+    reader = csv.DictReader(file)
+
+    for row in reader:
+        trial.append(int(row["Trial"]))
+        reactionTime.append(int(row["ReactionTime_ms"]))
