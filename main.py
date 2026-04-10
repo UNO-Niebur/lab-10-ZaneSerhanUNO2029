bc520b221dbc76cb227ad92261507f30bcd5cb25
ZaneSerhanUNO2029 <12106466@nebraska.edu>
2026-04-10 23:05:53 +0000

    in progress

--- main.py
+++ main.py
@@ -2,14 +2,27 @@
 #Name:
 #Date:
 #Assignment:
+
 import csv
-import matplotlib as plt
+import matplotlib.pyplot as plt
 trial = []
 reactionTime = []
+inFile = open("reaction_time_data.csv", "r")
+reader = csv.DictReader(inFile)
+for row in reader:
+    trial.append(int(row["Trial"]))
+    reactionTime.append(int(row["ReactionTime_ms"]))
 
-with open("reaction_time_data.csv", "r") as file:
-    reader = csv.DictReader(file)
+cleanTrial = []
+cleanReaction = []
 
-    for row in reader:
-        trial.append(int(row["Trial"]))
-        reactionTime.append(int(row["ReactionTime_ms"]))
+for i in range(len(reactionTime)):
+    if reactionTime[i] > 0:
+        cleanTrial.append(trial[i])
+        cleanReaction.append(reactionTime[i])
+
+plt.plot(cleanTrial, cleanReaction, marker='o')
+plt.xlabel("Trial Number")
+plt.ylabel("Reaction Time (ms)")
+plt.title("Reaction Time vs Trial Number")
+plt.savefig("output.png")
